4592707df9e6f202b0f668c7d3299b41aeac7ab2
Kenneth Gulbrandsøy <kenneth@discoos.org>
2014-11-30 12:42:25 +0100

    Refactored project structure. Added more leaflet controls

--- src/leaflet/platforms/android/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/kengu/src/git/cordova-leaflet-app/src/leaflet/platforms/android/ant-build/AndroidManifest.xml : \
-/home/kengu/src/git/cordova-leaflet-app/src/leaflet/platforms/android/AndroidManifest.xml \
--- src/leaflet/platforms/android/assets/www/js/index.js
+++ src/leaflet/platforms/android/assets/www/js/index.js
@@ -17,10 +17,12 @@
  * under the License.
  */
 var app = {
+
     // Application Constructor
     initialize: function() {
         this.bindEvents();
     },
+
     // Bind Event Listeners
     //
     // Bind any events that are required on startup. Common events are:
@@ -33,19 +35,124 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
+
         app.receivedEvent('deviceready');
+
+        app.map = app.createMap();
+
     },
+    // Create map with
+    //
+    // * base map (default: OSM)
+    // * zoom controls
+    // * edit controls
+    // * geo-search control (default: Google)
+    //
+    createMap: function() {
+
+        var map = L.map('map');
+
+        L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
+            maxZoom: 18,
+            attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+                '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+                'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+            id: 'examples.map-i875mjb7'
+        }).addTo(map);
+
+        new L.Control.GeoSearch({
+            provider: new L.GeoSearch.Provider.Google()
+        }).addTo(map);
+
+        // Initialise the FeatureGroup to store editable layers
+        var drawnItems = new L.FeatureGroup();
+		map.addLayer(drawnItems);
+
+		var drawControl = new L.Control.Draw({
+			draw: {
+				position: 'topleft',
+				polygon: {
+					title: 'Draw a sexy polygon!',
+					allowIntersection: false,
+					drawError: {
+						color: '#b00b00',
+						timeout: 1000
+					},
+					shapeOptions: {
+						color: '#bada55'
+					},
+					showArea: true
+				},
+				polyline: {
+					metric: false
+				},
+				circle: {
+					shapeOptions: {
+						color: '#662d91'
+					}
+				}
+			},
+			edit: {
+				featureGroup: drawnItems
+			}
+		});
+		map.addControl(drawControl);
+
+		new L.Draw.PolylineTouch(map).enable();
+
+		map.on('draw:created', function (e) {
+			var type = e.layerType,
+				layer = e.layer;
+
+			if (type === 'marker') {
+				layer.bindPopup('A popup!');
+			}
+
+			drawnItems.addLayer(layer);
+		});
+
+
+    /*
+        app.map.on('locationfound', onLocationFound);
+        app.map.on('locationerror', onLocationError);
+
+        app.map.locate({setView: true, maxZoom: 16});
+    */
+
+        return map;
+    },
+
     // Update DOM on a Received Event
     receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
+
+        /*var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
         var receivedElement = parentElement.querySelector('.received');
 
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
+        */
 
         console.log('Received Event: ' + id);
+
     }
+
+    /*
+    onLocationFound: function(e) {
+        var radius = e.accuracy / 2;
+
+        L.marker(e.latlng).addTo(app.map)
+            .bindPopup("You are within " + radius + " meters from this point").openPopup();
+
+        L.circle(e.latlng, radius).addTo(app.map);
+    }
+
+    onLocationError: function(e) {
+        alert(e.message);
+    }
+    */
+
+
 };
 
 app.initialize();
